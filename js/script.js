49cb31e19b4eb6de7d6fc389ac02383cc5f24ebc
mugeshkumar <kmugesh11199@gmail.com>
2022-08-24 16:32:20 +0530

    add new file

---
+++ js/script.js
@@ -0,0 +1,45 @@
+/*addition
+let a=10;
+let b=25;
+console.log(a+b);
+*/
+/*addition
+let a=20;
+let b=22;
+console.log(a+=b);*/
+/*multiplication
+let a=10;
+let b=30;
+console.log(a*=b);*/
+/*division
+let a=20;
+let b=10;
+console.log(a/=b);*/
+/*subtraction
+let a=30;
+let b=10;
+console.log(a-=b);*/
+/*calculate area of circle
+let A=Math.PI*Math.pow(5,2);
+console.log(A);*/
+/*Area of triangle
+let A=1/2*5*6;
+console.log(A);*/
+/*simple interest
+let S=30*12*(12/100);
+console.log(S);*/
+/*CALCULATE SQUARE//
+let S=Math.pow(10,2);
+console.log(S);*/
+/*calculate cube//
+let C=Math.pow(12,3);
+console.log(C);*/
+//swap two numbers//
+let a=10;
+let b=20;
+let c;
+c=a;
+a=b;
+b=c;
+console.log(a);
+console.log(b);
